f75b445525f1c0cbe30757f32f2ccbb5d0fe8828
jabba10 <sahrjabba100@gmail.com>
2025-10-09 00:05:13 +0300

    fix: add sitemap redirects to netlify config

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.professionalresumefree.com', // Replace with your actual domain
+  generateRobotsTxt: true, // generates robots.txt automatically
+  outDir: 'public', // outputs to public folder
+}
