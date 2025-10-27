e055704362c179fcad8c7bcef4a140c0a5579f70
jabba10 <sahrjabba100@gmail.com>
2025-10-27 20:01:06 +0200

    Update project files

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,20 @@
-// src/pages/_app.js (Recommended)
+
+// src/pages/_app.js
+import { useRouter } from 'next/router';
+import Head from 'next/head';
 import Navbar from '../Components/Navbar';
 import Footer from '../Components/Footer';
 import './globals.css';
 
-
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  const canonicalUrl = `https://www.professionalresumefree.com${router.asPath}`;
+
   return (
     <>
+      <Head>
+        <link rel="canonical" href={canonicalUrl} key="canonical" />
+      </Head>
       <Navbar />
       <main className="app-wrapper">
         <Component {...pageProps} />
