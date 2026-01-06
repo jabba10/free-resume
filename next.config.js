2f1c5d8b33ea0c024462029348a53e2ac82d6a8e
jabba10 <isatakamara1001@gmail.com>
2026-01-06 18:38:36 +0200

    this one here

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
-  // Remove the headers() function - it doesn't work with static exports
+  images: { unoptimized: true }
 }
-
-module.exports = nextConfig
+module.exports = nextConfig;
