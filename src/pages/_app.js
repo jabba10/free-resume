2d9480c52a164514f358814d8f6d06835eb9ba48
jabba10 <sahrjabba100@gmail.com>
2026-01-08 18:54:43 +0200

    b

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,8 @@
-
 // src/pages/_app.js
 import { useRouter } from 'next/router';
 import Head from 'next/head';
+import Script from 'next/script';
+import { useEffect } from 'react';
 import Navbar from '../Components/Navbar';
 import Footer from '../Components/Footer';
 import './globals.css';
@@ -9,12 +10,57 @@ import './globals.css';
 export default function App({ Component, pageProps }) {
   const router = useRouter();
   const canonicalUrl = `https://www.professionalresumefree.com${router.asPath}`;
+  
+  // Add this line - Environment variable with fallback
+  const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID || 'G-ZKH84N99Z2';
+
+  // Track page views on route changes
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      if (window.gtag) {
+        window.gtag('config', GA_MEASUREMENT_ID, {
+          page_path: url,
+        });
+      }
+    };
+    
+    // Track initial page load
+    handleRouteChange(router.asPath);
+    
+    // Track subsequent route changes
+    router.events.on('routeChangeComplete', handleRouteChange);
+    
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events, router.asPath, GA_MEASUREMENT_ID]); // Added GA_MEASUREMENT_ID to dependencies
 
   return (
     <>
       <Head>
         <link rel="canonical" href={canonicalUrl} key="canonical" />
       </Head>
+      
+      {/* Google Analytics Scripts */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      
       <Navbar />
       <main className="app-wrapper">
         <Component {...pageProps} />
